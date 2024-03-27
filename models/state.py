608b4a82a7b5ef76b59a49727c9a779e6f362e5d
rivero04 <7710@holbertonstudents.com>
2024-03-27 10:24:22 -0700

    fix circular import

--- models/state.py
+++ models/state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models import storage_type
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
@@ -8,6 +7,7 @@ from models.base_model import BaseModel, Base
 
 class State(BaseModel, Base):
     """Representation of state """
+    from models import storage_type
     if storage_type == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
@@ -15,14 +15,14 @@ class State(BaseModel, Base):
     else:
         name = ""
 
-    if storage_type != "db":
-        @property
-        def cities(self):
-            """Getter for a list of city instances associated with the state"""
-            from models.city import City
-            cities = []
-            all_cities = storage_type.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    cities.append(city)
-            return cities
+    @property
+    def cities(self):
+        """Getter for a list of city instances associated with the state"""
+        from models.city import City
+        from models import storage_type
+        cities = []
+        all_cities = storage_type.all(City)
+        for city in all_cities.values():
+            if city.state_id == self.id:
+                cities.append(city)
+        return cities
