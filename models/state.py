7fdd3ff48c2a29434715d5410bad67d54c8ebf87
rivero04 <7710@holbertonstudents.com>
2024-03-27 10:25:00 -0700

    return fixes

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
@@ -7,22 +8,21 @@ from models.base_model import BaseModel, Base
 
 class State(BaseModel, Base):
     """Representation of state """
-    from models import storage_type
-    if storage_type == "db":
+    if models.storage_type == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state", cascade="all, delete")
     else:
         name = ""
 
-    @property
-    def cities(self):
-        """Getter for a list of city instances associated with the state"""
-        from models.city import City
-        from models import storage_type
-        cities = []
-        all_cities = storage_type.all(City)
-        for city in all_cities.values():
-            if city.state_id == self.id:
-                cities.append(city)
-        return cities
+    if models.storage_type != "db":
+        @property
+        def cities(self):
+            """Getter for a list of city instances associated with the state"""
+            from models.city import City
+            cities = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    cities.append(city)
+            return cities
