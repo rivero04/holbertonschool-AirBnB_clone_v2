7ed0d7b911ea3dd4b24ea5fee59d87a9697f762c
rivero04 <7710@holbertonstudents.com>
2024-04-08 09:47:13 -0700

    Pycodestyle and Docstring

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,18 +4,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Base route that displays "Hello HBNB!"."""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Route that displays "HBNB"."""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """Route that displays "C " followed by the value of the text variable,
+    replacing underscores with spaces."""
     text = text.replace('_', ' ')
     return f"C {text}"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,27 +4,41 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Base route that displays "Hello HBNB!"."""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Route that displays "HBNB"."""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
+    """Route that displays "C " followed by the value of the text variable,
+    replacing underscores with spaces."""
     text = text.replace('_', ' ')
     return f"C {text}"
 
+
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
+    """Route that displays "Python "
+    followed by the value of the text variable,
+    replacing underscores with spaces."""
     text = text.replace("_", " ")
-    return ("Python {}".format(text))
+    return f"Python {text}"
+
 
 @app.route('/python/', strict_slashes=False)
 def python_cool():
-    return ("Python is cool")
+    """Route that displays "Python is cool"."""
+    return "Python is cool"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
